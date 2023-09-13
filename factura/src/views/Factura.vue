9e688ba3e66f30fea790e61411f764d3956c8fbc
MiguelLopezCa <migantlopez@uniboyaca.edu.co>
2023-09-12 20:13:35 -0500

    Creacion
    creacion

---
+++ factura/src/views/Factura.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="form">
+        
+        <h1>Formulario</h1>
+        <label>Articulo:</label>
+        <br>
+
+        <input type="text">
+        <br>
+        <label>Cantidad:</label>
+        <br>
+
+        <input type="text">
+        <br>
+
+        <label>Valor unitario:</label>
+        <br>
+        <input type="text">
+        <br>
+      <button @click="agregar">Agregar</button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import Tabla from './Tabla.vue';
+  export default {
+    data() {
+      return {
+        articulo: '',
+        cantidad: '',
+        valorUnitario: '',
+        articulos: []
+      };
+    },
+    components: { Tabla },
+    methods: {
+      agregar() {
+        const nuevoArticulos = {
+          articulo: this.articulo,
+          cantidad: this.cantidad,
+          valorUnitario: this.valorUnitario
+        };
+      }
+    }
+  };
+  </script>
+  
---
+++ factura/src/views/Tabla.vue
@@ -0,0 +1,33 @@
+<template>
+    <table class="tab">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Articulo</th>
+          <th>Cantidad</th>
+          <th>V.Unit</th>
+          <th>Total</th>
+          <th>Eliminar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(i, index) of articulos" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ i.articulo }}</td>
+          <td>{{ i.cantidad }}</td>
+          <td>{{ i.valorUnitario }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <script lang="ts">
+
+  export default {
+    props: {
+        articulos:Array
+    },
+    
+  };
+  </script>
+  
